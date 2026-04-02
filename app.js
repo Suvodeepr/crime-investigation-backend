b143d3c2619de5e966c57a2675da4f77af42dd96
Suvodeep Roy <121038485+Suvodeepr@users.noreply.github.com>
2026-04-02 22:06:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import connectdb from "./db/connectdb.js";
+import firroutes from "./routes/firroutes.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const database_url = process.env.database_url || "mongodb://localhost:27017";
+
+// connect database
+connectdb(database_url);
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/fir", firroutes);
+
+// server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
